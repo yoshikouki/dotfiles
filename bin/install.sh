b706feb5ce97ad17c91a2022b87a613a090f5c5f
yoshikouki <yoshikouki@gmail.com>
2020-10-14 00:04:28 +0900

    デプロイコマンドを作成（ホームディレクトリにリンクを貼る）

---
+++ bin/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dotfiles_dir=$(cd $(dirname $0)../; pwd)
+
+## Create symlink to home directory
+echo 'Start to deploy dotfiles to home directory.'
+echo '---------- Linked ----------'
+for file in .??*
+do
+    if [ "$file" == ".git" ] \
+    || [ "$file" == ".gitignore" ] \
+    || [ "$file" == ".DS_Store" ]; then
+      continue
+    fi
+    
+    ln -sfnv "$dotfiles_dir/$file" "$HOME/$file"
+done
+echo '---------- Done Linked ----------'
