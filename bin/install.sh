bc6833c47bcf05bde37c48f4f8608a512da99900
yoshikouki <yoshikouki@gmail.com>
2020-10-14 09:47:08 +0900

    記述を簡略化

--- bin/install.sh
+++ bin/install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 dotfiles_dir=$(cd "$(dirname "$0")"/../ || return; pwd)
+files='.??*'
+ignore_files=(.git .gitignore .DS_Store)
 
 ## Create symlink to home directory
 echo 'Start to deploy dotfiles to home directory.'
 echo '---------- Linked ----------'
-for file in .??*
+
+for file in $files
 do
-    if [ "$file" == ".git" ] \
-    || [ "$file" == ".gitignore" ] \
-    || [ "$file" == ".DS_Store" ]; then
-      continue
-    fi
-    
-    ln -sfnv "$dotfiles_dir/$file" "$HOME/$file"
+  if [[ "${ignore_files[@]}" =~ $file ]]; then
+    continue
+  fi
+
+  ln -sfnv "$dotfiles_dir/$file" "$HOME/$file"
 done
 echo '---------- Done Linked ----------'
