ed12442880774444fb143e2d02e57b623ad680c5
yoshikouki <yoshikouki@pepabo.com>
2020-10-17 17:17:16 +0900

    インストールコマンドの DL を修正

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 
 FILES='.??*'
 IGNORE_FILES=(.git .gitignore .DS_Store)
-GITHUB_URL=https://github.com/yoshikouki/dotfiles
+GITHUB_URL=github.com/yoshikouki/dotfiles
 DOTPATH=~/.dotfiles
 
 has_command() {
@@ -13,36 +13,40 @@ has_command() {
   fi
 }
 
-echo 'Start to deploy dotfiles to home directory.'
+echo 'Start to deploy dotfiles to home directory.'; echo ""
 
 # HTTP経由でのインストールを実装する
 echo '---------- Download dotfiles ----------'
 ## git が使える場合
 if has_command "git";then
   echo 'use git'
-  git clone --recursive "$GITHUB_URL.git" "$DOTPATH"
+  if [ -d "$DOTPATH" ]; then
+    git pull
+  else
+    git clone --recursive "$GITHUB_URL.git" "$DOTPATH"
+  fi
 
 ## curl / wget が使える場合
 elif has_command "curl" || has_command "wget"; then
-  tarball="$GITHUB_URL/archive/master.tar.gz"
+  tarball="$GITHUB_URL/archive/main.zip"
   if has_command "curl"; then
     echo 'use curl'
     curl -L "$tarball"
   elif has_command "wget"; then
     echo 'use wget'
     wget -O - "$tarball"
-  fi | tar zxv
-  mv -f dotfiles-master "$DOTPATH"
+  fi | tar zxv -C ~/
+  mv -iT ~/dotfiles-main "$DOTPATH"
 
 # どちらもない場合は終了する
 else
-  echo "curl or wget required"
+  echo "[ERROR] curl or wget required"
   exit
 fi
 
 ## ~/.dotfiles があるか確認
 cd "$DOTPATH" || { echo "not found: $DOTPATH"; exit 1; }
-echo '---------- Downloaded ----------'
+echo "---------- Downloaded ----------"; echo ""
 
 # ドットファイルのシンボリックリンクをホームディレクトリに配置する
 echo '---------- Create symbolic link  ----------'
