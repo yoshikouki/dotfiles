88654e101630f582f710dccb85933536e2ccd8d4
yoshikouki <yoshikouki@pepabo.com>
2020-10-17 17:19:56 +0900

    fix

--- bin/install.sh
+++ bin/install.sh
@@ -20,7 +20,7 @@ echo '---------- Download dotfiles ----------'
 ## git が使える場合
 if has_command "git";then
   echo 'use git'
-  if [ -d "$DOTPATH" ]; then
+  if cd "$DOTPATH"; then
     git pull
   else
     git clone --recursive "$GITHUB_URL.git" "$DOTPATH"
