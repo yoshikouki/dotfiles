1d8fffe44abf580041516e251eba827f92785d84
yoshikouki <yoshikouki@gmail.com>
2022-06-08 09:45:40 +0900

    macos 設定を追加

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -6,11 +6,16 @@ if [ "$(uname)" != "Darwin" ] ; then
 fi
 
 # キーリピートの高速化
-defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # マウスの速度を速める
+defaults write -g com.apple.trackpad.scaling 3
 defaults write -g com.apple.mouse.scaling 5
+
+## 三本指でドラッグ
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
 
 # スクロールバーの常時表示
 defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
@@ -24,11 +29,17 @@ defaults write com.apple.CrashReporter DialogType -string "none"
 # 時計アイコンクリック時にOSやホスト名ipを表示する
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
+## ウィンドウサイズを調整する際の加速再生
+defaults write -g NSWindowResizeTime -float 0.001
+
 # ターミナルでUTF-8のみを使用する
 defaults write com.apple.terminal StringEncodings -array 4
 
 # ターミナル終了時のプロンプトを非表示にする
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+## DNSを指定してネットが早くなってほしい。IPv4とIPv6
+networksetup -setdnsservers Wi-Fi 8.8.4.4 8.8.8.8 2001:4860:4860::8844 2001:4860:4860::8888
 
 # Dock ======================================================
 
@@ -49,8 +60,12 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # パスバーを表示
 defaults write com.apple.finder ShowPathbar -bool true
 
+## Finder のタイトルバーにフルパスを表示する
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true    
+
 # 名前で並べ替えを選択時にディレクトリを前に置くようにする
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
 # 検索時にデフォルトでカレントディレクトリを検索
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
