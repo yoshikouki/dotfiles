946de341269d40856f1104a1c0c11a947edebee2
yoshikouki <yoshikouki@gmail.com>
2026-02-02 22:46:37 +0900

    nvim: keep wrap enabled in normal buffers

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function enable_wrap()
+  if vim.bo.buftype ~= "" then
+    return
+  end
+  vim.opt_local.wrap = true
+  vim.opt_local.linebreak = true
+end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("AlwaysWrap", { clear = true }),
+  callback = enable_wrap,
+})
+
+vim.schedule(enable_wrap)
